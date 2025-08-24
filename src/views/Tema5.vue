fd0020f0d978830c98f82da8b4bafcdad9fcf6cf
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-23 22:11:58 -0500

    tema5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,9 +5,153 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Tema 5
+      h1 Estatutos nacionales de transporte
 
+    .row.justify-content-center.align-items-center
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src="@/assets/curso/temas/tema5/1.png" alt="Imagen decorativa")
+      .col-lg-8
+        p Los Estatutos Nacionales de Transporte son el conjunto de normas jurídicas, reglamentos, decretos y leyes que regulan de manera integral las actividades relacionadas con el transporte terrestre en el territorio colombiano. Estas disposiciones establecen los principios, competencias, obligaciones y derechos de los diferentes actores del sistema de transporte: conductores, propietarios, empresas, usuarios y autoridades. Su objetivo principal es garantizar la organización, eficiencia, seguridad, sostenibilidad y legalidad en la prestación de los servicios de transporte público y privado de personas y mercancías.
+        p Entre los estatutos más relevantes se encuentran:
+        ul.checklist.py-3.pe-4.bg-color-acento-ac5
+          li La Ley 336 de 1996, también conocida como el Estatuto Nacional del Transporte.
+          li El Código Nacional de Tránsito Terrestre (Ley 769 de 2002).
+          li Reglamentaciones del Ministerio de Transporte, la Superintendencia de Transporte, y normas emitidas por la Agencia Nacional de Seguridad Vial (ANSV).
+         
+    Separador
 
+    #t_5_1(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 5.1.	Normas según tipo de transporte
+
+    p.mb-4.mb-lg-5 En todas partes del mundo se estable una normatividad para garantizar la seguridad, contando por regulaciones especificas en cuento a su operación, permisos y equipamiento.
+
+    TabsC.color-acento-botones
+      .py-3.py-md-4(titulo="Vehículos particulares (autos, motocicletas, camionetas)")
+        .row.align-items-center.justify-content-center
+          .col-md-5.d-none.d-lg-block
+            figure
+              img.mb-4.mb-md-0(src='@/assets/curso/temas/tema5/2.png', alt='Automovil')
+          .col-lg-6.mb-4.mb-md-0.p-4
+            h4 Vehículos particulares (autos, motocicletas, camionetas):
+            p Normatividad: reglamento de Tránsito local (municipal, estatal o nacional).
+            p.mb-1 Requisitos:
+            ul.checklist.checklist--location
+              li Licencia vigente.
+              li Seguro obligatorio.
+              li Verificación vehicular, placas y tarjeta de circulación actualizadas.
+            p.mb-1 Normas específicas:
+            ul.checklist.checklist--location
+              li Uso del cinturón de seguridad.
+              li Límites de velocidad.
+              li Prohibición de uso del celular al conducir.
+
+      .py-3.py-md-4(titulo="Vehículos de transporte público (taxis, autobuses)")
+        .row.align-items-center.justify-content-center
+          .col-md-5.d-none.d-lg-block
+            figure
+              img.mb-4.mb-md-0(src='@/assets/curso/temas/tema5/3.png', alt='Automovil')
+          .col-lg-6.mb-4.mb-md-0.p-4
+            h4 Vehículos de transporte público (taxis, autobuses)
+            p Normatividad: Ley de Transporte Público y su reglamento correspondiente.
+            p.mb-1 Requisitos:
+            ul.checklist.checklist--location
+              li Licencia especial de conductor (tipo B, C, D o E según el país).
+              li Tarjeta de concesión o permiso de operación.
+              li Seguro de responsabilidad civil para pasajeros.
+              li Cumplimiento de rutas autorizadas y tarifas reguladas.
+            p.mb-1 Normas específicas:
+            ul.checklist.checklist--location
+              li Revisiones físico-mecánicas periódicas.
+              li Respeto de horarios y paradas.
+
+      .py-3.py-md-4(titulo="Vehículos de carga (camiones, tractocamiones, grúas)")
+        .row.align-items-center.justify-content-center
+          .col-md-5.d-none.d-lg-block
+            figure
+              img.mb-4.mb-md-0(src='@/assets/curso/temas/tema5/4.png', alt='Automovil')
+          .col-lg-6.mb-4.mb-md-0.p-4
+            h4 Vehículos de carga (camiones, tractocamiones, grúas)
+            p Normatividad: Ley de Caminos, Puentes y Autotransporte Federal (o equivalente en otros países).
+            p.mb-1 Requisitos:
+            ul.checklist.checklist--location
+              li Licencia especial de conductor tipo C, B o E.
+              li Permisos de carga (general o específica, como materiales peligrosos).
+              li Carta de porte para traslado de mercancías.
+            p.mb-1 Normas específicas:
+            ul.checklist.checklist--location
+              li Peso y dimensiones máximas permitidas.
+              li Señalización especial para cargas largas o peligrosas.
+              li Tiempos máximos de conducción y descanso para operadores.
+
+      .py-3.py-md-4(titulo="Vehículos especializados (ambulancias, transporte escolar)")
+        .row.align-items-center.justify-content-center
+          .col-md-5.d-none.d-lg-block
+            figure
+              img.mb-4.mb-md-0(src='@/assets/curso/temas/tema5/5.png', alt='Automovil')
+          .col-lg-6.mb-4.mb-md-0.p-4
+            h4 Vehículos especializados (ambulancias, transporte escolar)
+            p Normatividad: reglamentos específicos para servicio especializado.
+            p.mb-1 Requisitos:
+            ul.checklist.checklist--location
+              li Licencias especializadas.
+              li Certificados de habilitación y protocolos de seguridad.
+              li Equipamiento específico (luces, sirenas, botiquines).
+            p.mb-1 Normas específicas:
+            ul.checklist.checklist--location
+              li Prioridad de paso en emergencias.
+              li Mantenimiento preventivo obligatorio.
+              li Supervisión de condiciones de higiene y seguridad (en transporte escolar, por ejemplo).
+
+    Separador
+    #t_5_2(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 5.2.	Límite de velocidad
+
+    .bloque-texto-a.color-acento-contenido.p-4.p-md-5.mb-5
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-8.mb-4.mb-lg-0
+          .bloque-texto-a__texto.p-4
+            ul.checklist.checklist--location
+              li <strong>Vías urbanas</strong>: máx. 60 km/h.
+              li <strong>Zonas escolares y residenciales</strong>: máx. 30 km/h.
+              li <strong>Carreteras nacionales</strong>: máx. 80-100 km/h Estos pueden modificarse según señalización oficial.
+            p Todo depende del país donde establezcan las normas.
+        .col-lg-4.mb-4.mb-lg-0
+          figure
+            img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/6.png', alt='Imagen decorativa')
+
+    Separador
+
+    #t_5_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 5.3.	Tipo de infracción
+    
+    .row.justify-content-center.align-items-center
+      .col-lg-1.d-none.d-xl-block
+        figure
+          img.mb-4.mb-xl-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/9.svg', alt='Imagen decorativa')
+      .col-lg-7
+        p Los tipos de infracción se establece según los tipos: A, B, C, D Y E, desde las leves a las más graves.
+        ul.checklist.checklist--location
+          li Infracciones leves vial de seguridad: son faltas menores que no comprometen gravemente la seguridad vial, pero afectan el orden o las normas básicas.
+          li Infracciones graves: son conductas que afectan significativamente la seguridad o el flujo normal del transporte, y que pueden generar accidentes o desorden.
+          li Infracciones muy graves: son actos que ponen en riesgo directo y grave la vida de las personas, los bienes y el entorno vial.
+      .col-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/7.png', alt='Imagen decorativa')
+
+    Separador
+
+    #t_5_4(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 5.4.	Sanciones
+    
+    .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-8.mb-4.mb-lg-0
+          .bloque-texto-a__texto.p-4
+            p.mb-0 Estas corresponden a las faltas correspondiente y ser determina: multas, suspensión o cancelación de licencia, arresto en casos extremos, en Colombia una manera de preparar al ciudadano en no cometer más infracciones se recomienda realizar una capacitación que apoya a un descuento del 50 % en aquellas personas que deseen realizarlo.
+        .col-lg-4
+          figure
+            img.mb-4.mb-lg-0(data-aos="fade-down")(src='@/assets/curso/temas/tema5/8.png', alt='Imagen decorativa')
 </template>
 
 <script>
