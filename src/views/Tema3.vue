f82dbae6808a1831094259ea41c3997d92777a52
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-24 23:13:03 -0500

    tema 3

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,74 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Tema 3
+      h1 Educación vial
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src="@/assets/curso/temas/tema3/1.png" alt="Imagen decorativa")
+      .col-lg-8
+        .row.align-items-center
+          .col-lg-1.d-none.d-xl-block.p-0
+            figure
+              img(src='@/assets/curso/temas/tema3/2.svg', alt='icono decorativo')
+          .col-lg-11
+            p La educación vial es el proceso formativo, continuo y sistemático mediante el cual se transmiten conocimientos, valores, actitudes y comportamientos orientados a fomentar una convivencia segura, responsable y respetuosa en el espacio público vial. Su propósito es crear conciencia ciudadana sobre los derechos y deberes de todos los actores viales (peatones, ciclistas, conductores, pasajeros y autoridades), con el fin de prevenir accidentes, promover la seguridad vial y mejorar la movilidad en las vías terrestres.
+        .cajon.color-secundario.p-4
+          p.mb-0 La educación vial permite entender no solo las normas de tránsito, sino también los factores humanos, culturales y sociales que inciden en el comportamiento vial. A través de esta, se busca fortalecer competencias ciudadanas, técnicas y éticas, esenciales para el análisis, control y acompañamiento de las dinámicas del transporte terrestre. Esta formación abarca desde el conocimiento de señales de tránsito, normativas legales y uso correcto de la vía, hasta el desarrollo de habilidades para identificar riesgos, tomar decisiones adecuadas en situaciones de tránsito y fomentar una cultura de respeto, inclusión y corresponsabilidad en la vía pública.
+
+    Separador
+
+    .fullwidth-bg.fullwidth-bg--1
+    #t_3_1(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 3.1.	Principios y valores ciudadanos
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-lg-8
+        ul.checklist.checklist--location
+          li Respeto a la vida: prioriza la integridad y bienestar de todas las personas en la vía.
+          li <strong>Legalidad</strong>: promueve el cumplimiento de las normas de tránsito.
+          li <strong>Responsabilidad</strong>: cada usuario es responsable de su comportamiento en la vía.
+          li <strong>Solidaridad</strong>: implica empatía y apoyo mutuo entre usuarios de la vía.
+          li <strong>Prevención</strong>: se basa en anticipar riesgos para evitarlos.
+          li <strong>Tolerancia</strong>: capacidad para gestionar con equilibrio las diferencias de comportamiento en la vía, aceptando que los errores humanos son parte del sistema vial y deben afrontarse con criterio y autocontrol.
+          li <strong>Honestidad</strong>: actuación transparente y coherente con la legislación vial, incluso en ausencia de control externo, garantizando un entorno predecible y confiable para todos los usuarios.
+          li <strong>Cooperación</strong>: participación en la construcción de una cultura vial solidaria, promoviendo la interacción ordenada entre peatones, ciclistas, conductores y autoridades para mejorar la eficiencia y seguridad del sistema de transporte.
+
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema3/3.png', alt='imagen decorativa')
+
+    Separador
+
+    #t_3_2(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 3.2.	Convivencia vial
+
+    .bloque-texto-a.color-acento-contenido.p-4.p-md-5.mb-5
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-8.mb-4.mb-lg-0
+          .bloque-texto-a__texto.p-4
+            p Es el conjunto de prácticas, actitudes, normas y valores que regulan la interacción armónica, segura y respetuosa entre todos los actores del espacio público vial: peatones, conductores, ciclistas, motociclistas, pasajeros y autoridades. Implica reconocer que la vía es un espacio compartido, donde cada persona tiene derechos, deberes y responsabilidades, y en el que la cooperación mutua es esencial para prevenir conflictos, reducir accidentes y garantizar una movilidad eficiente y sostenible.
+        .col-lg-4.mb-4.mb-lg-0
+          figure
+            img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema3/13.png', alt='Imagen decorativa')
+    
+    p.mb-0 Por otro lado, se sustenta en valores ciudadanos como el respeto, la empatía, la cortesía, la tolerancia y la solidaridad, y se manifiesta a través de acciones como ceder el paso, cumplir con las señales, no obstruir el tráfico, evitar conductas agresivas y proteger a los actores más vulnerables (niños, adultos mayores, personas con discapacidad, ciclistas y peatones).
+
+    Separador
+
+    #t_3_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 3.3.	Hábitos durante el desplazamiento por las vías como conductor, peatón o pasajero
+
+    p.mb-4 Son los comportamientos repetitivos, conscientes o adquiridos que las personas adoptan al interactuar en el espacio vial, ya sea en su rol de conductores, peatones o pasajeros. Estos hábitos están directamente relacionados con el nivel de seguridad, responsabilidad, eficiencia y respeto que se manifiesta durante el tránsito por las vías terrestres.
+
+    .row.align-items-center.justify-content-center.bg-secundario-s5.p-4.py-xl-0
+      .col-lg-5
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema3/12.png', alt='Imagen decorativa')
+      .col-lg-7
+        p.mb-0 Los buenos hábitos, como respetar los límites de velocidad, usar el cinturón de seguridad, ceder el paso, cruzar por las cebras, evitar distracciones, usar transporte formal o no obstaculizar la vía, contribuyen a reducir la siniestralidad y mejorar la convivencia en el espacio público. Por el contrario, los malos hábitos; como el uso del celular mientras se conduce o camina, no respetar señales, subir o bajar del vehículo en lugares indebidos o invadir el carril contrario, generan conflictos, aumentan los riesgos y afectan negativamente el sistema de transporte.
+
 
 </template>
 
