9cc7fb79d88dc763a95573027757f51874de5263
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-26 00:01:51 -0500

    Assets tema 4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,103 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Tema 4
+      h1 Código Nacional de Tránsito
+
+    .row.align-items-center.justify-content-center.bg-secundario-s5.p-4.ps-xl-0.py-xl-0.mb-4.mb-lg-5
+      .col-lg-5.ps-xl-0
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema4/1.png', alt='Imagen decorativa')
+      .col-lg-7
+        p.mb-0 El Código Nacional de Tránsito Terrestre de Colombia es el marco legal que regula la circulación de personas, vehículos y animales por las vías públicas y privadas abiertas al público en el territorio nacional. Las funciones que lo rigen son: establecer normas de circulación de vehículos y peatones, regular la conducción, definir los requisitos para obtener la licencia y su renovación, prevenir accidentes de tránsito, determinar las funciones de las autoridades, imponer sanciones y multas, exigir seguros obligatorios como SOAT para proteger a las víctimas de accidentes aplicado para Colombia y regular los diferentes tipos de transporte.
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-lg-1.d-none.d-xl-block
+        figure
+          img.mb-4.mb-xl-0(data-aos="fade-up")(src='@/assets/curso/temas/tema5/9.svg', alt='Imagen decorativa')
+      .col-lg-6
+        p El código de tránsito incluye aspectos claves como:
+        .p-3.bg-color-acento-ac5
+          ul.checklist
+            li.py-1 Clasificación y señalización de vías.
+            li.py-1 Licencias de conducción y requisitos para circular.
+            li.py-1 Normas de comportamiento para conductores y peatones.
+            li.py-1 Régimen de infracciones y sanciones.
+            li.py-1 Requisitos técnicos y mecánicos de los vehículos.
+            li.py-1 Medidas de prevención de accidentes y promoción de la seguridad vial.
+      .col-10.col-lg-5
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema4/2.png', alt='Imagen decorativa')
+
+    .cajon.color-secundario.p-4.mb-4.mb-lg-5
+      p.mb-0 Las señales de tránsito son elementos fundamentales del sistema vial cuyo objetivo es regular, guiar e informar a los usuarios de las vías, promoviendo una movilidad segura, ordenada y eficiente. Están diseñadas conforme a normas técnicas establecidas por el Ministerio de Transporte. Su correcta interpretación y cumplimiento son esenciales para prevenir accidentes, proteger la vida de peatones y conductores, y garantizar el respeto por las normas que rigen la convivencia en el espacio público
+
+    p.mb-4.mb-lg-5 Las señales de transito se clasifican en 4 grupos:
+
+    TabsA.color-acento-botones
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Reglamentarias")
+        .row.justify-content-center
+          .col-md-6
+            h4 Reglamentarias
+            p Estas indican obligaciones o prohibiciones. Por ejemplo: límites de velocidad o señal de pare.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/3.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Preventivas")
+        .row.justify-content-center
+          .col-md-6
+            h4 Preventivas
+            p Ellas son alertas sobre posibles peligros o condiciones especiales en la vía, curvas, zonas escolares o intersecciones.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/4.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Informativas")
+        .row.justify-content-center
+          .col-md-6
+            h4 Informativas
+            p Brindan datos como direcciones, nombres de calles, hospitales, establecimientos de servicios, etc
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/5.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Transitorias")
+        .row.justify-content-center
+          .col-md-6
+            h4 Transitorias
+            p Son señales temporales que se utilizan en zonas de obras o mantenimiento vial.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/6.png', alt='Imagen decorativa')
+
+    Separador
+
+    .fullwidth-bg.fullwidth-bg--1
+    #t_4_1(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.1.	Principios
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-lg-8
+        p Los principios del Código Nacional de Tránsito Terrestre de Colombia, establecidos en la Ley 769 de 2002, constituyen el fundamento normativo y ético que guía la regulación del comportamiento de todos los actores viales en el país. Estos principios orientan la aplicación de las normas de tránsito con el fin de garantizar la seguridad vial, proteger la vida y la integridad física de las personas, conservar el medio ambiente, y asegurar una movilidad eficiente, equitativa y ordenada.
+        p Principios fundamentales del Código Nacional de Transito:
+        ul.checklist.checklist--location
+          li La seguridad de las personas como prioridad en la movilidad.
+          li La protección del medio ambiente mediante un tránsito sostenible y responsable.
+          li La eficiencia del transporte, promoviendo el uso racional de las vías y los recursos públicos.
+          li El respeto por los derechos humanos, especialmente de los usuarios más vulnerables: peatones, ciclistas, personas con movilidad reducida, niños y adultos mayores.
+          li La corresponsabilidad entre el Estado, los usuarios de la vía y las autoridades de tránsito en la prevención de siniestros y la educación vial.
+          li La legalidad, como principio rector para aplicar sanciones, regular comportamientos y definir competencias institucionales.
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema4/7.png', alt='imagen decorativa')
+
+    Separador
+
+    #t_4_2(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.2.	Autoridades
+
+    .cajon.color-acento-contenido.p-4.mb-4.mb-lg-5
+      p.mb-0 Las autoridades del Código Nacional de Tránsito Terrestre son las entidades públicas y funcionarios legalmente facultados para planear, regular, controlar, sancionar y vigilar las actividades relacionadas con la circulación de personas, vehículos y animales por las vías del territorio colombiano, de acuerdo con lo establecido en la Ley 769 de 2002 y sus reformas. Estas autoridades desempeñan un papel esencial en el funcionamiento y supervisión del sistema de movilidad, pues garantizan que las normas de tránsito se cumplan, que los derechos y deberes de los actores viales se respeten, y que se promueva una movilidad segura, ordenada y sostenible.
 
 
 </template>
