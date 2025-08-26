3745e922a9ec026e263a403a0e93288d4cd6b4a8
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-08-26 14:47:44 -0500

    completar tema 4

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -103,7 +103,291 @@
     .cajon.color-acento-contenido.p-4.mb-4.mb-lg-5
       p.mb-0 Las autoridades del Código Nacional de Tránsito Terrestre son las entidades públicas y funcionarios legalmente facultados para planear, regular, controlar, sancionar y vigilar las actividades relacionadas con la circulación de personas, vehículos y animales por las vías del territorio colombiano, de acuerdo con lo establecido en la Ley 769 de 2002 y sus reformas. Estas autoridades desempeñan un papel esencial en el funcionamiento y supervisión del sistema de movilidad, pues garantizan que las normas de tránsito se cumplan, que los derechos y deberes de los actores viales se respeten, y que se promueva una movilidad segura, ordenada y sostenible.
 
+    .row.justify-content-center.align-items-center.z-2
+      .col-lg-3.d-none.d-lg-block
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema4/8.png', alt='imagen decorativa')
+      .col-lg-9
+        AcordionA(tipo="b" clase-tarjeta="tarjeta tarjeta--gris")
+          .row(titulo="El Ministerio de Transporte")
+            .col-md-6.mb-4.mb-md-0
+              p Entidad rectora del tránsito en el país, encargada de formular políticas, reglamentar la normatividad y coordinar el sistema nacional.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/9.png', alt='Imagen decorativa')
 
+          .row(titulo="Los organismos de tránsito")
+            .col-md-6.mb-4.mb-md-0
+              p Dependencias municipales, distritales o departamentales responsables del control directo en cada jurisdicción (por ejemplo, Secretaría de Movilidad). 
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/10.png', alt='Imagen decorativa')
+
+          .row(titulo="La Policía Nacional (Dirección de Tránsito y Transporte)")
+            .col-md-6.mb-4.mb-md-0
+              p Autoridad encargada del control operativo del tránsito en las vías nacionales y en municipios sin organismo propio.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/11.png', alt='Imagen decorativa')
+
+          .row(titulo="Los alcaldes y gobernadores")
+            .col-md-6.mb-4.mb-md-0
+              p Como primeras autoridades en su territorio, pueden establecer medidas de regulación del tránsito local conforme a la ley.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/12.png', alt='Imagen decorativa')
+
+          .row(titulo="Inspectores de tránsito o agentes de tránsito")
+            .col-md-6.mb-4.mb-md-0
+              p Funcionarios que aplican directamente la normatividad, imponen comparendos y desarrollan labores pedagógicas y preventivas.
+            .col-md-6
+              figure
+                img(src='@/assets/curso/temas/tema4/13.png', alt='Imagen decorativa')
+
+    Separador
+
+    #t_4_3(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.3.	Registro de Información
+
+    .row.justify-content-center.align-items-center.z-2
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema4/14.png', alt='imagen decorativa')
+      .col-lg-8
+        .cajon.color-secundario.p-4.mb-4
+          p.mb-0 Es el proceso sistemático de capturar, organizar, almacenar y actualizar datos relevantes sobre las operaciones, eventos o condiciones que ocurren durante el desarrollo del transporte terrestre. Este proceso puede realizarse de forma manual o digital, y su correcta ejecución permite generar trazabilidad, tomar decisiones informadas, prevenir incidentes y optimizar los recursos logísticos y humanos involucrados en la movilidad vial.
+        .row.justify-content-center.align-items-center
+          .col-lg-9
+            p Este proceso también es clave para alimentar sistemas tecnológicos como el RUNT (Registro Único Nacional de Tránsito), bases de datos locales de los organismos de tránsito, plataformas de monitoreo satelital (GPS/GNSS), y reportes operativos que permiten establecer tendencias, patrones y áreas de mejora.          
+          .col-lg-3.d-none.d-lg-block
+            figure
+              img(src='@/assets/curso/temas/tema4/15.png', alt='Imagen decorativa')
+
+    Separador
+
+    #t_4_4(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.4.	Licencias
+
+    .bloque-texto-a.color-acento-contenido.p-4.p-md-5.mb-5
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-8.mb-4.mb-lg-0
+          .bloque-texto-a__texto.p-4
+            p Este documento permite acreditar el tipo de vehículo a opera, cumplir con las condiciones técnicas y mecánicas adecuadas. En Colombia, las licencias de conducción se clasifican en categorías A, B y C, dependiendo del tipo de vehículo que se desee conducir y si es para servicio particular o público. Las categorías A son para motocicletas, las B para vehículos particulares (autos, camionetas, etc.), y las C para vehículos de servicio público.
+        .col-lg-4.mb-4.mb-lg-0
+          figure
+            img.mb-4.mb-lg-0(data-aos="fade-up")(src='@/assets/curso/temas/tema4/16.png', alt='Imagen decorativa')
+
+    Separador
+
+    #t_4_5(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.5.	Condiciones del vehículo
+
+    p Las condiciones del vehículo hacen referencia al estado técnico, mecánico, estructural y funcional de todos los sistemas, componentes y elementos que conforman un automotor, garantizando que esté en capacidad óptima para circular de manera segura, eficiente y conforme a la normativa legal vigente. Estas condiciones abarcan aspectos clave como el sistema de frenos, dirección, suspensión, luces, llantas, motor, carrocería, entre otros.
+
+    TabsA.color-acento-botones.mb-4.mb-lg-5
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de frenos")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de frenos
+            p La función del sistema de frenos es reducir la velocidad, detener o inmovilizar el vehículo con seguridad. Su buen funcionamiento es esencial para proteger al conductor, pasajeros y demás usuarios de la vía. Está compuesto por:
+            ul.checklist.checklist--location
+              li Pedal de freno.
+              li Bomba de freno.
+              li Líquido de frenos.
+              li Discos y tambores.
+              li Pastillas o zapatas.
+              li Mordazas o <em>calipers</em>.
+              li Freno de parqueo.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/17.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de dirección")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de dirección
+            p El sistema de dirección permite controlar la trayectoria del vehículo, guiando las ruedas delanteras mediante el volante. Asegura maniobrabilidad, estabilidad y seguridad, e incluye mecanismos mecánicos, hidráulicos o eléctricos según el tipo de vehículo. Ésta compuesto por:
+            ul.checklist.checklist--location
+              li Volante.
+              li Columna de dirección.
+              li Caja de dirección.
+              li Barra de dirección.
+              li Terminales de dirección.
+              li Brazos o rótulas.
+              li Amortiguador de dirección (en algunos vehículos).
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/18.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de suspensión")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de suspensión
+            p Conjunto de componentes mecánicos y elásticos que conectan las ruedas del vehículo con su chasis o carrocería, permitiendo absorber impactos y mantener la estabilidad, el confort y la seguridad durante la conducción. Está compuesto por:
+            ul.checklist.checklist--location
+              li Amortiguadores.
+              li Resortes o muelles.
+              li Barras estabilizadoras.
+              li Brazos de suspensión.
+              li Rótulas y bujes.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/19.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de Luces")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de Luces
+            p Conjunto de dispositivos eléctricos diseñados para iluminar la vía, mejorar la visibilidad y comunicar las acciones del conductor a otros usuarios de la carretera. Es fundamental para la seguridad activa, especialmente en condiciones de poca luz o clima adverso. Está compuesto por:
+            ul.checklist.checklist--location
+              li Luces delanteras.
+              li Luces traseras.
+              li Direccionales o intermitentes.
+              li Luces de emergencia.
+              li Luz de niebla.
+              li Iluminación interior y del tablero.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/20.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de llantas")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de llantas
+            p Garantiza el contacto entre el vehículo y la superficie de la vía. Está compuesto por las llantas (neumáticos) y los rines, cumpliendo funciones clave como soportar el peso del vehículo, absorber impactos del terreno, transmitir la tracción, el frenado y la dirección. 
+            p Los componentes son:
+            ul.checklist.checklist--location  
+              li Neumático o llanta.
+              li Rin.
+              li Válvula.
+              li Banda de rodadura.
+              li Costados o flancos.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/21.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema del motor")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema del motor
+            p Componentes mecánicos, eléctricos y electrónicos que generan generar la energía necesaria para mover un vehículo. Su función es convertir la energía del combustible (gasolina, diésel o energía eléctrica, según el tipo de motor) en energía mecánica, para así impulsar el automóvil a través del sistema de transmisión. Se encuentra integrado por:
+            ul.checklist.checklist--location  
+              li Bloque motor.
+              li Pistones.
+              li Cigüeñal.
+              li Bielas.
+              li Culata.
+              li Válvulas.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/22.png', alt='Imagen decorativa')
+
+      .tarjeta.bg-color-acento-ac5.p-5(titulo="Sistema de carrocería")
+        .row.justify-content-center
+          .col-md-6
+            h4 Sistema de carrocería
+            p Es la estructura externa del vehículo que cubre y protege tanto a los ocupantes como a los componentes mecánicos y eléctricos. Su diseño no solo cumple funciones estéticas, sino también estructurales, de seguridad, aerodinámica y confort. Sus partes son:
+            ul.checklist.checklist--location  
+              li Capó.
+              li Techo.
+              li Puertas: permiten el acceso al habitáculo.
+              li Guardabarros.
+              li Parachoques.
+              li Cofre y baúl.
+              li Ventanas y parabrisas
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema4/23.png', alt='Imagen decorativa')
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-11
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 2.
+          span Estructura de la normativa vial
+        img.mb-1.d-sm-none(data-aos="fade-down")(src="@/assets/curso/temas/tema4/24-mob.png", alt="Estructura de la normativa vial. La figura muestra son los componentes que hacen parte del cumplimiento de la Normatividad vial y que significa cada uno")
+        img.mb-1.d-none.d-md-block(data-aos="fade-down")(src="@/assets/curso/temas/tema4/24.png", alt="Estructura de la normativa vial. La figura muestra son los componentes que hacen parte del cumplimiento de la Normatividad vial y que significa cada uno")
+        figcaption <strong>Fuente:</strong> SENA, 2025.
+
+    .fullwidth-bg.fullwidth-bg--1
+    Separador
+
+    #t_4_6(data-aos='zoom-in-right').titulo-segundo.color-acento-contenido.z-2
+      h3 4.6.	Seguros y responsabilidades del vehículo
+
+    p.mb-4.mb-lg-5.z-2 Los seguros son contratos que transfieren el riesgo económico derivado de posibles accidentes, daños o pérdidas, desde el propietario del vehículo hacia una compañía aseguradora. En Colombia, el seguro obligatorio más representativo es el SOAT (Seguro Obligatorio de Accidentes de Tránsito), que cubre daños corporales a ocupantes y terceros, independientemente de quién tenga la culpa en un accidente. Además del SOAT, existen seguros voluntarios que cubren aspectos como daños materiales, robo, responsabilidad civil extracontractual, carga transportada, entre otros. Estos son fundamentales en operaciones logísticas, de carga o transporte de pasajeros, donde se manejan altos niveles de riesgo.
+
+    .row.justify-content-center.align-items-center.z-2.mb-4.mb-lg-5
+      .col-10.col-sm-6.col-lg-4
+        figure
+          img(data-aos="fade-up")(src='@/assets/curso/temas/tema4/25.png', alt='imagen decorativa')
+      .col-lg-8
+        p Responsabilidad: responde civilmente por daños ocasionales a terceros (persona -bienes), documento obligatorio.
+        ul.checklist.checklist--location
+          li SOAT o equivalente.
+          li Seguro de responsabilidad civil extracontractual de mayor cobertura.
+          li Seguro de carga: permite proteger la mercancía de robo, daño o perdida.
+          li Póliza de transporte: nacional o internacional, según rutas.
+          li RC (Responsabilidad civil) contractual y extracontractual de amplio espectro.
+
+    .titulo-sexto.color-acento-contenido.mb-4
+      h5 Tabla 1.
+      span Estructura de la normativa vial
+    .tabla-a.color-acento-contenido
+      table
+        caption <strong>Fuente</strong>: SENA, 2025.
+        thead
+          tr(style="background-color: #EFF4FB")
+            th 
+            th 
+              figure
+                img.w-54(src='@/assets/curso/temas/tema4/26.svg', alt='Imagen decorativa')
+              h5 Vehículo particular
+            th
+              figure
+                img.w-54(src='@/assets/curso/temas/tema4/27.svg', alt='Imagen decorativa')
+              h5 Vehículo comercial
+            th
+              figure
+                img.w-54(src='@/assets/curso/temas/tema4/28.svg', alt='Imagen decorativa')
+              h5 Vehículo público
+            th
+              figure
+                img.w-54(src='@/assets/curso/temas/tema4/29.svg', alt='Imagen decorativa')
+              h5 Transporte especial
+            th
+              figure
+                img.w-54(src='@/assets/curso/temas/tema4/30.svg', alt='Imagen decorativa')
+              h5 Motocicletas
+        tbody
+          tr
+            td Seguro obligatorio
+            td SOAT
+            td SOAT o equivalente
+            td SOAT
+            td SOAT
+            td SOAT
+          tr(style="background-color: #F6F6F6")
+            td Seguro de responsabilidad
+            td Cubre daños a terceros.
+            td Cobertura extendida.
+            td Contractual y extra contractual.
+            td Póliza contractual.
+            td Cubre daños a terceros.
+          tr
+            td Seguros adicionales
+            td Todo riesgo, asistencia en carretera.
+            td Seguro de carga, póliza de transporte.
+            td Accidente personal, perdida de ganancias.
+            td Riesgos especiales, pólizas ambientales.
+            td Cubre daños a tercero.
+          tr(style="background-color: #F6F6F6")
+            td Responsabilidades
+            td Responsabilidad civil por daños.
+            td Perdida o daño de mercancías.
+            td Cumplimiento de condiciones de seguridad.
+            td Cumplimiento de regulaciones estrictas. 
+            td Daños personales/materiales
+    
 </template>
 
 <script>
